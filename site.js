ae592c73dcb93a0c7c08cb8c4a166b9b67af3ad7
TretornESP <damebandakl@gmail.com>
2021-02-07 14:50:30 +0100

    Add files via upload

---
+++ site.js
@@ -0,0 +1,81 @@
+
+      const url = 'http://musicbrainz.org/ws/2/artist/?query=artist:equilibrium&fmt=json';
+      var online = true;
+
+      function createNode(element) {
+        return document.createElement(element);
+      }
+
+
+      function append(parent, el) {
+        return parent.appendChild(el);
+      }
+
+      function changeLanguage(language) {
+          var element = document.getElementById("url");
+          element.value = language;
+          element.innerHTML = language;
+      }
+
+      function showDropdown() {
+          document.getElementById("myDropdown").classList.toggle("show");
+      }
+
+      function toggleId() {
+        var divsToHide = document.getElementsByClassName("id");
+        var visib;
+        if (document.getElementById("show_id").checked) {
+          visib = "visible";
+        } else {
+          visib = "collapse";
+        }
+        for (var i = 0; i < divsToHide.length; i++) {
+          divsToHide[i].style.visibility = visib;
+        }
+      }
+
+      function ffetchQuery() {
+        console.log("EXEC: ");
+        const ul = document.getElementById('datatab');
+        fetch (url)
+        .then((resp) => resp.json())
+        .then(function(data) {
+          let artists = data.artists;
+            return artists.map(function(artist) {
+              let li = createNode('li');
+              let id = createNode('span');
+              let name = createNode('span');
+              id.innerHTML = `${artist.id}`;
+              id.classList.add("id");
+              name.innerHTML = `${artist.name}`;
+              append(li, id);
+              append(li, name);
+              append(ul, li);
+            })
+        }).catch(function(error) {
+          console.log(error);
+        });
+      }
+
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(event) {
+          if (!event.target.matches('.dropbtn')) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+                  var openDropdown = dropdowns[i];
+                  if (openDropdown.classList.contains('show')) {
+                      openDropdown.classList.remove('show');
+                  }
+              }
+          }
+      }
+
+      window.addEventListener('load', function () {
+        if (online) {
+          ffetchQuery();
+        } else {
+          console.log("OFFLINE");
+          //Cache thinge
+        }
+      })
